2a8b9df97490af3597cd563cbc25701f3bcd413d
anandhperumal3 <97460597+anandhperumal3@users.noreply.github.com>
2022-06-09 13:50:52 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ import os
 class PrivateAIApp(L.LightningFlow):
     def __init__(self) -> None:
         super().__init__()
-        pai_access_token = os.environ['PrivateAI_key']
+        pai_access_token = 'INTERNAL_TESTING_UNLIMITED_REALLY'
         self.pai_sythetic_data_generator = PrivateAISyntheticData(
             key=pai_access_token,
             mode='standard',
-            text_features='review',
-            url="http://localhost:8080/deidentify_text"
+            output_path='./',
+            host='localhost',
+            port='8080'
         )
+        self.pai_sythetic_data_generator.server_started = True
 
     def run(self):
         self.pai_sythetic_data_generator.run('I work at Private AI and I live in Vancouver.')
